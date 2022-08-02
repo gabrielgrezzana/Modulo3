2ed50db1e7fbe24af819fa2d7e4907874a5e0179
gabrielgrezzana <106358234+gabrielgrezzana@users.noreply.github.com>
2022-08-01 22:55:04 -0300

    modulo3

---
+++ mocks/animes.js
@@ -0,0 +1,50 @@
+const animes = [
+  {
+    id: "2145abc",
+    title: "Naruto",
+    protagonist: "Naruto",
+    gender: "Shounen",
+    year: 2009,
+    characters: [
+      {
+        name: "Sasuke",
+        lastName: "Uchiha",
+        skill: "charingan",
+        age: 16,
+        gender: "Male",
+      },
+      {
+        name: "Shikamaro",
+        lastName: "Nara",
+        skill: "Kagemane",
+        age: 16,
+        gender: "Male",
+      },
+    ],
+  },
+  {
+    id: "214765abc",
+    title: "Dragon Ball Z",
+    protagonist: "Goku",
+    gender: "Shounen",
+    year: 1998,
+    characters: [
+      {
+        name: "Vegeta",
+        lastName: "unknow",
+        skill: "Sayajin",
+        age: 25,
+        gender: "Male",
+      },
+      {
+        name: "Majimboo",
+        lastName: "unknow",
+        skill: "Eat",
+        age: 3000,
+        gender: "Male",
+      },
+    ],
+  },
+];
+
+module.exports = animes;
